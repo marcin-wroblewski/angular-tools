2587896cb0cdfcb624afa27e8f628c792c02c011
MWR <MWR>
2017-05-24 08:36:15 +0200

    modul

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,23 +5,18 @@ import { HttpModule } from '@angular/http';
 import { DataTableModule } from 'primeng/primeng'
 
 import { AppComponent } from './app.component';
-import { TableComponent } from './table/table.component';
-import { RowComponent } from './table/row.component'
-import { Column, ColumnBodyTemplateRenderer, MWTemplate  } from './table/column.component';
-// import { MwTableModule } from './table/mw-table.module'
+import { MwTableModule } from './table/mw-table.module'
 
 @NgModule({
   declarations: [
     AppComponent,
-    TableComponent, RowComponent,
-    Column, ColumnBodyTemplateRenderer, MWTemplate
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
     DataTableModule,
-    // MwTableModule,
+    MwTableModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/table/mw-table.module.ts
+++ src/app/table/mw-table.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { TableComponent } from './table.component';
 import { RowComponent } from './row.component'
-import { Column, ColumnBodyTemplateRenderer, MWTemplate  } from './column.component';
+import { Column, ColumnBodyTemplateRenderer, MWTemplate } from './column.component';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    TableComponent, RowComponent,
-    Column, ColumnBodyTemplateRenderer, MWTemplate
-  ],
-  declarations: []
+    imports: [
+        CommonModule,
+        FormsModule
+    ],
+    exports: [
+        TableComponent,
+        Column,
+        MWTemplate
+    ],
+    declarations: [
+        TableComponent,
+        RowComponent,
+        Column,
+        ColumnBodyTemplateRenderer,
+        MWTemplate
+    ]
 })
 export class MwTableModule { }
